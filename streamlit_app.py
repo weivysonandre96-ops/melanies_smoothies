b9a73fb5581cfd81baf29b8e994318599a50a825
weivysonandre96-ops <weivysonandre96@gmail.com>
2025-11-22 21:47:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,13 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
-#new section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+
+#new section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
