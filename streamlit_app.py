3026b1f14225cc73c139de7d552b0aa1e0b1236e
weivysonandre96-ops <weivysonandre96@gmail.com>
2025-11-22 21:49:32 -0300

    Update streamlit_app.py
    identando

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 #new section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+  import requests
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  st.text(smoothiefroot_response)
