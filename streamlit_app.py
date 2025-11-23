fc275d7c6497d49a77b066cf5bff65fbdafb9707
weivysonandre96-ops <weivysonandre96@gmail.com>
2025-11-22 21:49:56 -0300

    Update streamlit_app.py
    identando 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ if ingredients_list:
 
 #new section to display smoothiefroot nutrition information
   import requests
-  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  st.text(smoothiefroot_response)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
